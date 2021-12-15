411aaf778b2b8e3fe820ef4a036a8e3272c3573f
mo33q <mo33q@umsystem.edu>
2021-12-14 20:01:37 -0600

    Added Javascript
    Added javascript, updated map

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
 
+  expandImg.src = jpegs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
